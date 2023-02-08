66eb1fb4088ab40688f1bc4328829f4ba64c9b23
iamtheak <np03cs4a220100@heraldcollege.edu.np>
2023-02-08 10:14:02 +0545

    Portfolio complete webpages

---
+++ portfolio webpages/javscript/mode.js
@@ -0,0 +1,8 @@
+function mode() {
+    const links = document.getElementById("links");
+    if (links.style.display === "block") {
+      links.style.display = "none";
+    } else {
+      links.style.display = "block";
+    }
+  }
---
+++ week-nine/js/index.js
@@ -0,0 +1,6 @@
+alert("hola amigo")
+console.log("que?")
+var paragraph = document.getElementById("paragraph")
+lol = document.querySelector("#paragraph").innerHTML
+paragraph.innerHTML = "OMG lebron james"
+paragraph.style.background = "green";
